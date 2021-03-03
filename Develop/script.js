b8a8d0ace484017382ee258c73776bdc8df74140
Freddie Brewin <freddiebrewin@live.com>
2021-03-03 14:16:27 +0000

    Creating form and button that shows and hides the form

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var myForm = document.getElementById("password")
 
-  passwordText.value = password;
+
+// Write password to the #password input
+
+function showForm() { //This function will show the user the form and then create the arrays that will store the values given by the user
+  if(myForm.style.display == 'block'){
+    myForm.style.display = "none"
+  }
+  else  {
+    myForm.style.display ="block"
+  }
+}
+
+function generatePassword() { //This function will randomly pick an element out of the previous array and place it into a new array
+  
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+/* criteriaBtn.addEventListener("click", showForm); */
+
+/* generateBtn.addEventListener("click", generatePassword); */
+
